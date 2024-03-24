3f0992d757b2c425514b8ab3d73ebf2d0eeb0625
arifizzu <arif.izzu97@gmail.com>
2024-03-25 07:47:12 +0800

    Edit FieldSeeder

--- database/seeders/FieldSeeder.php
+++ database/seeders/FieldSeeder.php
@@ -20,34 +20,58 @@ class FieldSeeder extends Seeder
             'type_id' => 1,
             'is_required' => true,
             'column_name' => 'test column 1',
-            'width' => 30,
-            'height' => 10,
-            'x_coordinate' => 100,
-            'y_coordinate' => -50,
+            'width' => 3,
+            'height' => 3,
+            'x_coordinate' => 0,
+            'y_coordinate' => 0,
         ]);
 
         Field::create([
-            'caption' => 'Checkbox Caption',
+            'caption' => 'Email Caption',
             'form_id' => 1,
-            'type_id' => 4,
+            'type_id' => 11,
             'is_required' => true,
             'column_name' => 'test column 2',
-            'width' => 10,
-            'height' => 5,
-            'x_coordinate' => 150,
-            'y_coordinate' => -200,
+            'width' => 4,
+            'height' => 4,
+            'x_coordinate' => 3,
+            'y_coordinate' => 0,
         ]);
 
         Field::create([
-            'caption' => 'Checkbox Caption',
+            'caption' => 'Password Caption',
+            'form_id' => 1,
+            'type_id' => 12,
+            'is_required' => true,
+            'column_name' => 'test column 3',
+            'width' => 4,
+            'height' => 4,
+            'x_coordinate' => 3,
+            'y_coordinate' => 3,
+        ]);
+
+        Field::create([
+            'caption' => 'Date Caption',
             'form_id' => 2,
-            'type_id' => 4,
+            'type_id' => 9,
+            'is_required' => false,
+            'column_name' => 'test column 1',
+            'width' => 3,
+            'height' => 3,
+            'x_coordinate' => 0,
+            'y_coordinate' => 0,
+        ]);
+
+         Field::create([
+            'caption' => 'Time Caption',
+            'form_id' => 2,
+            'type_id' => 10,
             'is_required' => false,
             'column_name' => 'test column 3',
-            'width' => 10,
-            'height' => 5,
-            'x_coordinate' => 150,
-            'y_coordinate' => -200,
+            'width' => 3,
+            'height' => 3,
+            'x_coordinate' => 0,
+            'y_coordinate' => 3,
         ]);
     }
 }
