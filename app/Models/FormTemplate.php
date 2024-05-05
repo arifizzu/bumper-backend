a5aaeb86315a788c34e71e376ba0cd4e3ebd9895
arifizzu <arif.izzu97@gmail.com>
2024-05-05 12:38:34 +0800

    Fix formtemplated model

--- app/Models/FormTemplate.php
+++ app/Models/FormTemplate.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 
 // use App\Models\Form;
@@ -20,8 +20,8 @@ class FormTemplate extends Model
         'form_id',
     ];
 
-    public function form(): BelongsTo
+    public function form(): HasMany
     {
-        return $this->belongsTo(Form::class);
+        return $this->hasMany(Form::class, 'id');
     }
 }
